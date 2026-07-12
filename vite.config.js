61c6525bcfad68cb0280ae1ac18cbedb1e5ce12d
rares1205 <56553962+rares20070215@users.noreply.github.com>
2026-07-12 19:04:06 +0300

    -'

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/scoala-oniceni/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
